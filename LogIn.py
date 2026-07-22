563adb08194d9d9bb25fa71baea8f38064328e7b
Shreyansh Singh Developes <shrey.code.python@gmail.com>
2026-07-22 09:53:09 +0000

    Here is The Code 🙂

---
+++ LogIn.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root= Tk()
+root.title("Log-In")
+root.geometry("300×600")
+root.config(bg="lightblue")
+root.resizable(False, False)
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
